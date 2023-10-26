769b8325355d3749814ca7bf867b02b207048afc
Jayaraghavan <jayaraghavan@Jayaraghavans-MacBook-Pro.local>
2023-10-27 00:39:48 +0530

    added scripts and consolidated to trigger.bash

--- indent.py
+++ indent.py
@@ -6,6 +6,6 @@ regex_compile =  re.compile(regex)
 file_list     =  subprocess.run("ls",text=True,stdout=subprocess.PIPE).stdout
 for files in file_list.splitlines():
     if(regex_compile.search(files)):
-        with open(files,"w+") as file_handle:
+        with open(files,"r+") as file_handle:
             json_dict = json.load(file_handle)
             json.dump(json_dict,file_handle,indent=2)
